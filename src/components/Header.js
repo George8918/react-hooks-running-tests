107f3d718b5f3467108fcf47645bf1d45be68b9d
George <GZheng8918@gmail.com>
2023-05-20 16:02:57 -0700

    complete lab

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+import Article from "../components/Article";
 import React from "react";
+
+
+test("displays the text 'please pass this test'", () => {
+  render(<Article />);
+
+  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+});
+
 
 function Header(props) {
       return (
-        <h1>
-          {/* uncomment the line below to pass the test */}
-          {/* hello from the Header! */}
-        </h1>
+        <h1> hello from the Header!</h1>
       )
 }
 
